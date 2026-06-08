a452bdc9f53dc30b99c69b57d4706ad3365d0758
Yulia Kozak <ykozak74@gmail.com>
2026-06-08 12:28:25 +0300

    start

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,18 @@
 // app/layout.tsx
+import Header from "@/src/components/Header/Header";
+import { Geist, Geist_Mono } from "next/font/google";
 
-import TanStackProvider from "@/src/components/TanStackProvider/TanStackProvider.jsx";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+import TanStackProvider from "@/src/components/TanStackProvider/TanStackProvider";
 
 // інший код файлу
 
